e4c7aa9d0aab27a9eebc38eed6617c89db72e608
prajwalmw <prajwalwaingankar@yahoo.in>
2020-08-29 20:07:04 +0530

    Home Screen UI is designed.

---
+++ app/src/main/java/com/prajwal/prajwalwaingankar_roz/Home_Screen/HomeScreen_Activity.kt
@@ -0,0 +1,11 @@
+package com.prajwal.prajwalwaingankar_roz.Home_Screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen_)
+    }
+}
