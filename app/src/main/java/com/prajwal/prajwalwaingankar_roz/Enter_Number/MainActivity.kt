f02cea70768d6d80e233989fad8c1e0ea977dcbd
prajwalmw <prajwalwaingankar@yahoo.in>
2020-08-29 21:45:29 +0530

    Enter number code is implemented.

--- app/src/main/java/com/prajwal/prajwalwaingankar_roz/Enter_Number/MainActivity.kt
+++ app/src/main/java/com/prajwal/prajwalwaingankar_roz/Enter_Number/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.prajwal.prajwalwaingankar_roz.Enter_Number
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.prajwal.prajwalwaingankar_roz.R
+import com.prajwal.prajwalwaingankar_roz.Verification_OTP.Verification_OTPActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_phone_number)
+        val button: Button = findViewById(R.id.button_continue)
+
+
+        button.setOnClickListener(View.OnClickListener
+        {
+            var mobileString: String = editText.text.toString().trim()
+
+            if(mobileString.isEmpty() || mobileString.length < 10) {
+                editText.setError("Enter a valid mobile number")
+                editText.requestFocus()
+            }
+
+            val intent = Intent(this, Verification_OTPActivity::class.java)
+            intent.putExtra("mobile", mobileString)
+            startActivity(intent)
+        })
 
     }
 
