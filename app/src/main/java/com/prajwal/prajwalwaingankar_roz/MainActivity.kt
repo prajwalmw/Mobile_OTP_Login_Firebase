d12683b0522fa336cba533ebfa391984e9d717bf
prajwalmw <prajwalwaingankar@yahoo.in>
2020-08-29 19:40:28 +0530

    Enter Number screen UI is designed.

--- app/src/main/java/com/prajwal/prajwalwaingankar_roz/MainActivity.kt
+++ app/src/main/java/com/prajwal/prajwalwaingankar_roz/MainActivity.kt
@@ -2,10 +2,14 @@ package com.prajwal.prajwalwaingankar_roz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.PhoneAuthProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
