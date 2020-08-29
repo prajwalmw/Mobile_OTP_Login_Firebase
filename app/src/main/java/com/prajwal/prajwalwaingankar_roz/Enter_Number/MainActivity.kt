b60d6818d0d3660d6174e0e1b75a3508a7d856a1
prajwalmw <prajwalwaingankar@yahoo.in>
2020-08-29 20:03:48 +0530

    OTP verification screen UI is designed.

--- app/src/main/java/com/prajwal/prajwalwaingankar_roz/Enter_Number/MainActivity.kt
+++ app/src/main/java/com/prajwal/prajwalwaingankar_roz/Enter_Number/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.prajwal.prajwalwaingankar_roz
+package com.prajwal.prajwalwaingankar_roz.Enter_Number
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.PhoneAuthProvider
+import com.prajwal.prajwalwaingankar_roz.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/prajwal/prajwalwaingankar_roz/Verification_OTP/Verification_OTPActivity.kt
@@ -0,0 +1,11 @@
+package com.prajwal.prajwalwaingankar_roz.Verification_OTP
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Verification_OTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification__o_t_p)
+    }
+}
